eea5dfa15caa030f30ce04bc82413eaeaf59564f
fojtik.jachym.2017 <fojtik.jachym.2017@UC404PC16>
2020-10-02 13:41:49 +0200

    Tridy

---
+++ Banka/Studentsky.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banka
+{
+    class Studentsky: Sporici
+    {
+        private bool vyberDnes = false;
+        public Studentsky(string jmeno, decimal z)
+        {
+            Jmeno = jmeno;
+            Zustatek = z;
+        }
+        public Studentsky(string jmeno)
+        {
+            Jmeno = jmeno;
+            Zustatek = 0;
+        }
+    }
+}
